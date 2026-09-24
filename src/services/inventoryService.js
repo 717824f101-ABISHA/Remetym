120124e310698251ad734a742873dfcb60996d8e
Abisha S <717824f101@kce.ac.in>
2026-09-24 15:44:16 +0530

    Update API base URL to production endpoint

--- src/services/inventoryService.js
+++ src/services/inventoryService.js
@@ -7,7 +7,7 @@
 import { KEYS } from '../utils/storageUtils';
 import { matchesDistrict } from '../utils/districtUtils';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://remetym-1.onrender.com/api';
 
 let inventoryCache = null;
 let lastInvFetch = 0;
