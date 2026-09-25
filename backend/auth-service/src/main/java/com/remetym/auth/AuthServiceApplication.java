f7138ce595096e15e43a811f7f5c1f8a3cecf389
Abisha S <717824f101@kce.ac.in>
2026-09-25 14:41:53 +0530

    Enable asynchronous processing in AuthServiceApplication

--- backend/auth-service/src/main/java/com/remetym/auth/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/remetym/auth/AuthServiceApplication.java
@@ -3,10 +3,13 @@ package com.remetym.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAsync
 public class AuthServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
